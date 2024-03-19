243730582497f1eec044d8800366e371f70e7de1
KiratDh <164057724+KiratDh@users.noreply.github.com>
2024-03-19 12:57:48 -0400

    Add files via upload

---
+++ Unknown.bat
@@ -0,0 +1,18 @@
+﻿@echo off
+msg * Warning your computer has detected a virus.
+msg * To remove the virus click OK or close this box
+msg * Your Hard drives are now being formatted
+msg * Please wait ...........
+msg * Shutting down in...
+msg * 10
+msg * 9
+msg * 8
+msg * 7
+msg * 6
+msg * 5
+msg * 4
+msg * 3
+msg * 2
+msg * 1
+msg * ...
+msg * APRIL FOOLS!!!!
